f370c12f77ecee8c730bfd6ac01a6a3aabedcce7
BerniceChua <Bernice.Chua.415@gmail.com>
2017-01-22 16:25:42 -0800

    Add code for cellular automata.

---
+++ Assets/_Scripts/MapGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapGenerator : MonoBehaviour {
+
+    public int width;
+    public int height;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    [Range(0, 100)] public int randomFillPercent;
+
+    public int mapSmoother = 5;
+
+    int[,] map;
+
+	// Use this for initialization
+	void Start () {
+        GenerateMap();
+	}
+	
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            GenerateMap();
+        }
+    }
+
+	void GenerateMap() {
+        map = new int[width, height];
+        RandomFillMap();
+
+        for (int i = 0; i < mapSmoother; i++) {
+            SmoothMap();
+        }
+    }
+
+    void RandomFillMap() {
+        if (useRandomSeed) {
+            seed = Time.time.ToString();
+        }
+
+        System.Random pseudoRNG = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1 ) {
+                    map[x, y] = 1;
+                } else {
+                    map[x, y] = (pseudoRNG.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    void SmoothMap() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int neighborWallTiles = GetSurroundingWallCount(x, y);
+
+                if (neighborWallTiles > mapSmoother-1) {
+                    map[x, y] = 1;
+                } else if (neighborWallTiles < mapSmoother-1) {
+                    map[x, y] = 0;
+                }
+            }
+        }
+    }
+
+    int GetSurroundingWallCount(int gridX, int gridY) {
+        int wallCount = 0;
+
+        for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++) {
+            for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++) {
+                if (neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height) {
+                    if (neighborX != gridX || neighborY != gridY) {
+                        wallCount += map[neighborX, neighborY];
+                    }
+                } else {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+    private void OnDrawGizmos() {
+        if (map != null) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-width/2 + x + 0.5f, 0, -height/2 + y + 0.5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+    }
+}
