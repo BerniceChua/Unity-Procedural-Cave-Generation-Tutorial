90d78b5a0ef24d73869822c303e4b28f4ae81477
BerniceChua <Bernice.Chua.415@gmail.com>
2017-01-22 18:20:50 -0800

    Add MeshGenerator.cs.

---
+++ Assets/_Scripts/MeshGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour {
+
+    public SquareGrid squareGrid;
+
+    public void GenerateMesh(int[,] map, float squareSize) {
+        squareGrid = new SquareGrid(map, squareSize);
+    }
+
+    public class SquareGrid { // holds 2D array of Squares
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float SquareSize) {
+
+        }
+    }
+
+    public class Square {
+        public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+        public Node centerTop, centerRight, centerBottom, centerLeft;
+
+        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft) {
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomRight = _bottomRight;
+            bottomLeft = _bottomLeft;
+
+            centerTop = topLeft.right;
+            centerRight = bottomRight.above;
+            centerBottom = bottomLeft.right;
+            centerLeft = bottomLeft.above;
+        }
+    }
+
+    public class Node {
+        public Vector3 position;
+        public int vertexIndex = -1;
+
+        public Node(Vector3 _pos) {
+            position = _pos;
+        }
+    }
+
+    public class ControlNode : Node {
+        public bool active; // if active, it's a wall.
+        public Node above, right;
+
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos) {
+            active = _active;
+            above = new Node(position + Vector3.forward * squareSize/2.0f);
+            above = new Node(position + Vector3.right * squareSize/2.0f);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
