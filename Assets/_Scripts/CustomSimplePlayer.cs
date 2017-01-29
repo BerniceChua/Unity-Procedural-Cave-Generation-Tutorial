512bf13b0d7d8c138f3c2b2921c54187c42da518
BerniceChua <Bernice.Chua.415@gmail.com>
2017-01-29 14:57:58 -0800

    Add 'CustomSimplePlayer' controller.

---
+++ Assets/_Scripts/CustomSimplePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomSimplePlayer : MonoBehaviour {
+    public int speed = 10;
+
+    Rigidbody rigidbody;
+    Vector3 velocity;
+
+    // Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed;
+	}
+
+    private void FixedUpdate() {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
