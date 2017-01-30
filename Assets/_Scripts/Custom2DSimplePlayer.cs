09124df969f80ed09a3e39fbd0f7bb72808954ca
BerniceChua <Bernice.Chua.415@gmail.com>
2017-01-29 16:01:54 -0800

    Create 2D version of Scene.

---
+++ Assets/_Scripts/Custom2DSimplePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom2DSimplePlayer : MonoBehaviour {
+    public int speed = 10;
+
+    Rigidbody2D rigidbody;
+    Vector2 velocity;
+
+    // Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed;
+	}
+
+    private void FixedUpdate() {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
