8886b0bac06ef0b37f8cae4e8ec503f07fe0f3e2
BerniceChua <Bernice.Chua.415@gmail.com>
2017-01-30 05:10:39 -0800

    Add code to detect if game is 2D or 3D, and code to disable/enable components as needed.

--- Assets/_Scripts/Detect2DOr3D.cs
+++ Assets/_Scripts/Detect2DOr3D.cs
@@ -7,12 +7,15 @@ using UnityEngine.Events;
 public class ToggleEvent : UnityEvent<bool> { }
 
 public class Detect2DOr3D : MonoBehaviour {
-    [SerializeField] ToggleEvent onToggleScript;
-    
     public GameObject m_caveMesh;
     public GameObject m_mainCamera;
     public GameObject m_fpsCamera;
+
+    //public CustomSimplePlayer script3D;
+    //public Custom2DSimplePlayer script2D;
     //public GameObject m_mapGenerator;
+
+    public GameObject m_playerController;
 
     // Use this for initialization
     void Start () {
@@ -23,10 +26,18 @@ public class Detect2DOr3D : MonoBehaviour {
         if (m_caveMesh.transform.rotation.eulerAngles.x == 270) {
             m_fpsCamera.SetActive(false);
             m_mainCamera.SetActive(true);
-            //m_mapGenerator.GetComponent<>
-        } else {
+
+            m_playerController.AddComponent<CapsuleCollider2D>();
+            m_playerController.AddComponent<Rigidbody2D>();
+            m_playerController.AddComponent<Custom2DSimplePlayer>();
+        } else if (m_caveMesh.transform.rotation.eulerAngles.x == 0) {
             m_fpsCamera.SetActive(true);
             m_mainCamera.SetActive(false);
+
+            m_playerController.AddComponent<CapsuleCollider>();
+            m_playerController.AddComponent<Rigidbody>();
+            m_playerController.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            m_playerController.AddComponent<CustomSimplePlayer>();
         }
     }
 }
